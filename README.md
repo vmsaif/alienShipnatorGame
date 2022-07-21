e562c9976a73fdca8563c21871333afdd1290e25
Saif Mahmud <60409889+vmsaif@users.noreply.github.com>
2022-07-21 14:40:42 +0600

    added some comments to change the alien bullet.

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ To play,
 
 Compile and run "Display.java" to launch the game from the src folder.
 
+You can change the alien bullet. Just uncomment the sprite from the src/AlienBuller.java to your preference.
+
 What I have learned:<br>
 Using 2d array color blocks to make games.
--- src/AlienBullet.java
+++ src/AlienBullet.java
@@ -6,32 +6,34 @@ import java.awt.Color;
 
 public class AlienBullet extends Sprite{
 	public static final Color [][] ALIEN_BULLET = 
-//		{
-//				{null, null, null, Display.BLUE, Display.BLUE },
-//				{null, null, Display.BLUE, Display.BLUE, null },
-//				{null, Display.BLUE, Display.BLUE, null, null },
-//				{Display.BLUE, Display.BLUE, null, null, null },
-//				{Display.BLUE, Display.BLUE, null, null, null },
-//				{null, Display.BLUE, Display.BLUE, null, null },
-//				{null, null, Display.BLUE, Display.BLUE, null },
-//				{null, null, null, Display.BLUE, Display.BLUE },
-//				{null, null, null, Display.BLUE, Display.BLUE },
-//				{null, null, Display.BLUE, Display.BLUE, null },
-//				{null, Display.BLUE, Display.BLUE, null, null },
-//				{Display.BLUE, Display.BLUE, null, null, null },
-//		};
+
+			//throw thunder 
+	//		{
+	//				{null, null, null, Display.BLUE, Display.BLUE },
+	//				{null, null, Display.BLUE, Display.BLUE, null },
+	//				{null, Display.BLUE, Display.BLUE, null, null },
+	//				{Display.BLUE, Display.BLUE, null, null, null },
+	//				{Display.BLUE, Display.BLUE, null, null, null },
+	//				{null, Display.BLUE, Display.BLUE, null, null },
+	//				{null, null, Display.BLUE, Display.BLUE, null },
+	//				{null, null, null, Display.BLUE, Display.BLUE },
+	//				{null, null, null, Display.BLUE, Display.BLUE },
+	//				{null, null, Display.BLUE, Display.BLUE, null },
+	//				{null, Display.BLUE, Display.BLUE, null, null },
+	//				{Display.BLUE, Display.BLUE, null, null, null },
+	//		};
 			
-			 //throw alien
-		{
-				{null, null, Display.BLACK, null, null, null, null, null, Display.BLACK, null, null},
-				{null, null, null, Display.BLACK, null, null, null, Display.BLACK, null, null, null},
-				{null,null,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,null,null},
-				{null,Display.BLACK,Display.BLACK,null,Display.BLACK,Display.BLACK,Display.BLACK,null,Display.BLACK,Display.BLACK,null},
-				{Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK},
-				{Display.BLACK,null,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,null,Display.BLACK},
-				{Display.BLACK,null,Display.BLACK,null,null,null,null,null,Display.BLACK,null,Display.BLACK},
-				{null,null,null,Display.BLACK,Display.BLACK,null,Display.BLACK,Display.BLACK,null,null,null}
-		};
+			//throw alien
+	{
+			{null, null, Display.BLACK, null, null, null, null, null, Display.BLACK, null, null},
+			{null, null, null, Display.BLACK, null, null, null, Display.BLACK, null, null, null},
+			{null,null,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,null,null},
+			{null,Display.BLACK,Display.BLACK,null,Display.BLACK,Display.BLACK,Display.BLACK,null,Display.BLACK,Display.BLACK,null},
+			{Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK},
+			{Display.BLACK,null,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,Display.BLACK,null,Display.BLACK},
+			{Display.BLACK,null,Display.BLACK,null,null,null,null,null,Display.BLACK,null,Display.BLACK},
+			{null,null,null,Display.BLACK,Display.BLACK,null,Display.BLACK,Display.BLACK,null,null,null}
+	};
 	
 	public AlienBullet(int x, int y) {
 		this.setxDot(x);
